b9ea849d6681391dbc4a581df9ac5a44d5d8791d
reuke <chuzovp@gmail.com>
2018-05-16 12:09:14 +0700

    Fix typo duplication

--- payload/gameRender.js
+++ payload/gameRender.js
@@ -178,46 +178,6 @@ window.gameFunctions.gameRender = function(){
 		draw.endFill();
 	}
 	
-	var updateLaser = function() {
-		if(!game.activePlayer || !game.activePlayer.container)
-			return;
-		
-		var laser = window.gameVars.Game.Laser;
-		
-		var draw = laser.draw;
-		
-		if(!draw)
-		{
-			draw = new window.PIXI.Graphics();
-			
-			laser.draw = draw;
-			game.activePlayer.container.addChild(draw);
-			game.activePlayer.container.setChildIndex(draw, 0);
-		}
-		
-		if(!draw.graphicsData)
-			return;
-		
-		draw.clear();
-		
-		if(!laser.active || !window.menu.UserSetting.shoot.lasersightEnabled)
-			return;
-		
-		var center = {x: 0, y: 0}
-		var radius = laser.range;
-		var angleFrom = laser.direction - laser.angle;
-		var angleTo = laser.direction + laser.angle;
-		
-		angleFrom = angleFrom > Math.PI * 2 ? angleFrom - Math.PI * 2 : angleFrom < 0 ? angleFrom + Math.PI * 2 : angleFrom;
-		angleTo = angleTo > Math.PI * 2 ? angleTo - Math.PI * 2 : angleTo < 0 ? angleTo + Math.PI * 2 : angleTo;
-		
-		draw.beginFill( 0xff0000, 0.1 );
-		draw.moveTo(center.x,center.y);
-		draw.arc(center.x, center.y, radius, angleFrom, angleTo);
-		draw.lineTo(center.x, center.y);
-		draw.endFill();
-	}
-	
 	var updateEnemyLines = function() {
 		if(!game.activePlayer || !game.activePlayer.container)
 			return;
