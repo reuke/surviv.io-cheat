7a48d952345425b3b3c786ed63231193ea0eddc0
reuke <chuzovp@gmail.com>
2018-05-20 15:33:33 +0700

    Cleanup

--- payload/gameSetting.js
+++ payload/gameSetting.js
@@ -1,16 +1,5 @@
 window.gameSetting = {};
 
-// window.gameSetting.Zoom = {
-	// ZoomEnabled: true,
-	// ZoomLevel: 0.5,
-// };
-
-// window.gameSetting.Alpha = {
-	// Obstacles: true,
-	// Buildings: true,
-	// Smoke: true,
-// };
-
 window.gameSetting.UserSetting = {
 	
 };
